6ec35e47f90ae033b209c775d3d231961ba7345a
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-07-31 20:57:41 +0200

    Getting the last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,5 +7,5 @@ if last_digit > 5:
 if last_digit == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
 if number < 6 and number != 0:
-    last_digit = number % 100
+    last_digit = number // 10
     print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
