c7e92b1702d41b60abf77e0174efa6cc97e9d2c1
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-02 00:33:40 +0200

    Functions are very usefull

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def convert_to_celsius(fahrenheit):
-    sum = (fahrenheit - 32) * 5/9
+def add(a, b):
+    sum = a + b
     return sum
---
+++ python-functions/3-string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def reverse_string(string)
+    result = int(string[-1])
+    return result
