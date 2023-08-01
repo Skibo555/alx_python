e2e50588508d8757dac2eca2029489eec8700ede
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-01 04:15:13 +0200

    Combinations of numbers

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 for i in range(9):
+    print("{:d}".format(i) end="")
     for j in range(10):
+        print("{:d}{:d}".format(j) end="")
         if i < j:
-             print("{:d}{:d}".format(i,j))
-        else:
             print("{:d}, {:d}".format(i,j), end=", ")
+        else:
+            print("{:d}, {:d}".format(i,j))
