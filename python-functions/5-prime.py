420514b7984bfef13e6fb84aac4a0a3fb037bbad
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-04 04:30:21 +0200

    Prime Number

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -5,5 +5,5 @@ def is_prime(number):
     for i in range(2, int(number ** 0.5) + 1):
         if number % i == 0:
             return False
-        else:
-            return True
+    else:
+        return True
