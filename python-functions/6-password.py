01ad12b36d00a0d4dfa210c0a8f805affbb6bf93
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-04 07:59:32 +0200

    Prime Number

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 def validate_password(password):
     if len(password) >= 8:
-        for char in password:
-            if any(char.issupper()):
-                for char in password:
-                    if any(char.islower()):
-                        for char in password:
-                            if any(char.isspace()):
-                                return True
+        return True
+    for char in password:
+        if any(char.issupper()):
+            return True
+    for char in password:
+        if any(char.islower()):
+            return True
+    for char in password:
+        if any(char.isspace()):
+            return True
     else:
         return False
