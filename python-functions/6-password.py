d031ed38fa27ca9de17a6674102beced9e14f704
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-04 08:11:56 +0200

    Password Validation

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python
 def validate_password(password):
-    if len(password) >= 8:
-        return True
-    for char in password:
-        if any(char.issupper()):
-            return True
-    for char in password:
-        if any(char.islower()):
-            return True
-    for char in password:
-        if any(char.isspace()):
-            return True
+    if len(password) > 7:
+        if not any(char.isupper() for char in password):
+            if not any(char.islower() for char in password):
+                if ' ' in password:
+                    return True
+
     else:
         return False
