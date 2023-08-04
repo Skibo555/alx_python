d2b521a2a595d3fd47e9f4a5564d973e0fa03dc7
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-04 04:02:00 +0200

    Prime Number

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 def is_prime(number):
-    if number / 1 == 1:
-        return True
-    if number / number == 1:
+    if number / 1 == 1 or number / 1 == number and number / number == 1:
         return True
     if number <= 0:
         return False
