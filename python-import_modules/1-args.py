8da73724337f12ac7f970821c0f6be5d1cbf68a9
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-08 15:05:14 +0200

    Modules in Py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     argument_lenth = len(sys.argv) - 1
     print("{:d} argument:".format(argument_lenth))
     for arg in sys.argv[1:]:
-        for i in sys.argv:
-            print("{}: {}".format(int(i), arg))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, arg))
     if argument_lenth > 0:
         print(".")
