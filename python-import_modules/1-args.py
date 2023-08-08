56676509701aee316b97fcc0853a93140f304d7b
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-08 13:49:22 +0200

    Modules in Py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argument_lenth = len(sys.argv) - 1
-    for arg in int(argument_lenth):
-        print("{:d}: {}".format(argument_lenth, arg))
+
+    argument_lenth = len(sys.argv)
+    print("{:d} argument:".format(argument_lenth))
+    for argument_lenth = len(sys.argv) - 1:
+    print("{:d}: {}".format(argument_lenth, sys.argv))
     if argument_lenth > 0:
-        print("{:d} argument:".format(argument_lenth))
-    else:
         print(".")
