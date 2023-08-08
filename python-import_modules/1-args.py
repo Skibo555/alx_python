f2b49e7beaea45bad14d565a5ecd4b95d0ed9aac
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-08 20:50:08 +0200

    Password Validation

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     argument_lenth = len(sys.argv) - 1
     if argument_lenth < 1:
         print("{:d} argument.".format(argument_lenth))
-    print("{:d} argument:".format(argument_lenth))
+        print("{:d} argument:".format(argument_lenth))
     for i in range(1, len(sys.argv)):
         for arg in sys.argv[1:]:
             print("{}: {}".format(i, arg))
