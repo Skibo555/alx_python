685bca283e276d28d3135a0683176a17f6eccda7
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-09 12:00:52 +0200

    Modules in Py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -4,6 +4,6 @@ def safe_print_division(a, b):
     try:
         a = a / b
     except ZeroDivisionError:
-        return(a)
+        return("None")
     finally:
         print("Inside result: {}".format(a))
