bc80db4009a134756248fa6540580a08a4a876d8
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-10 00:24:52 +0200

    we raising an exception for we are exceptional

---
+++ python-import_modules/5-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class CustomError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+def raise_exception_msg(message=""):
+    if message is not str:
+        raise NameError
