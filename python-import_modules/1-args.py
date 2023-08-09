543c639cc5379a5c7c9b4f3b22b480e4401ed0a1
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-09 04:23:50 +0200

    Modules in Py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -8,9 +8,9 @@ if argument_length == 1:
     print("{:d} argument:".format(argument_length))
     for i, arg in enumerate(sys.argv[1:], start=1):
         print("{}: {}".format(i, arg))
-#if argument_length == 0:
-    #print("{:d} arguments.".format(argument_length))
-elif argument_length > 1 or argument_length == 0:
+if argument_length == 0:
+    print("{:d} arguments.".format(argument_length))
+elif argument_length > 1:
     print("{:d} arguments:".format(argument_length))
     for i, arg in enumerate(sys.argv[1:], start=1):
         print("{}: {}".format(i, arg))
