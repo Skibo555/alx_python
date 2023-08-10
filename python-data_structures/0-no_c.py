bc41da9d344d8d9755b05f526b008ffb5ed48c20
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-11 01:08:00 +0200

    Working with a tuple

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -4,7 +4,7 @@ def no_c(my_string):
     filtered_string = ""
     for char in my_string:
         if char not in ['[o]', '[O]', '[C]', '[c]']:
-            filtered_string += char
+            filtered_string = my_string
         elif  char not in ['[C]', '[c]']:
-            filtered_string += char
+            filtered_string = my_string
         return filtered_string
