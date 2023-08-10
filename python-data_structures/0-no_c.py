2ed8c7072ca549738a9de0365dd840d80ae431f2
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-11 00:05:20 +0200

    Working with a tuple

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    if not ('[c-C]') in my_string:
+    if not ('[c-C]') and ('[o-O]') in my_string:
         new_string = my_string
         return new_string
