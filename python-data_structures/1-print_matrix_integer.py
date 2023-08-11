c929edcfd913275ad7ed95be80304db5a987dbaf
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-12 00:15:49 +0200

    Working with a tuple

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    matrix = [[1] + 1 for i in range(10)]
-    for i in range(10):
-        for j in range(10):
-            matrix[i][j] = "{:d} {:d}",str.format(i, j)
-
-    for i in range(10):
-        for j in range(10):
-            print(matrix[i][j], end='')
-        print()
+    for i in range(1, 4):
+        matrix = list(range(1, 10))
+        print("{:d}".format(i[:]))
+    return matrix
