ff498ab5b63246ffe665ae4d364907dde76e473a
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-11 07:37:26 +0200

    Working with a tuple

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    filtered_string = ""
+    new_string = ""  # Initialize an empty string to store the modified result
+    
     for char in my_string:
-        if char ['[o]', '[O]',] or ['[C]', '[c]'] not in my_string:
-            filtered_string = my_string
-        return filtered_string
+        # Check if the character is a lowercase or uppercase 'c'
+        if char not in ['c', 'C']:
+            new_string += char  # Add the character to the modified string
+    
+    return new_string  # Return the modified string
