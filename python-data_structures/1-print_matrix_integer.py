d2eb61a11c52a0520bbd0b63e9110666457b11c7
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-11 08:19:02 +0200

    Working with a tuple

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,4 +3,4 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):  # Iterate over the indices of rows in the matrix
         matrix[i] = list(range(1, 4))  # Replace each row with [1, 2, 3]
-        print(" ".join(map(str, matrix[i])))  # Print the modified row
+        print("{:d}",str.format(matrix[i]).join(map(str, matrix[i])))  # Print the modified row
