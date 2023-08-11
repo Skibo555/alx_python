d814e8725a6c056d203c0106134298b37842450a
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-11 19:28:50 +0200

    Working with a tuple

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{num:3}".format(row) end=" ")  # Adjust the formatting as needed
+            print("{:d}".format(row) end=" ")  # Adjust the formatting as needed
         print()  # Move to the next line after each row
