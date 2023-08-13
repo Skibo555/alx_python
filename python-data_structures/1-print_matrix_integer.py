a9d374f71590f5eee5ec0ae8bdbd83d55b24a8ed
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-13 05:17:09 +0200

    Working with a tuple

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,7 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for index, element in row:
+        for index, element in enumerate(row):
             print("{:d}".format(element), end="")
             if index < len(row) - 1:
                 print(" ", end="")
