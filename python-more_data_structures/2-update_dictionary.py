3271bb4ce0bdfcc1997e3ebe81221c8ba2ceb1a4
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-14 07:48:58 +0200

    Mapping in py

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    for key, value in a_dictionary.items():
+    for key, value in a_dictionary.items([]):
         return key, value
