c7568ca7bec5396925bc4822c73b3547d6f5d7c5
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-15 08:19:13 +0200

    Dictionary manupulations in py

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,3 +1,3 @@
 def multiply_list_map(my_list=[], number=0):
-    new_list = list(map(lambda x, y: my_list * number, my_list, number))
+    new_list = list(map(lambda x, y, z = [] : x * z + y * z, my_list, number,))
     return new_list
