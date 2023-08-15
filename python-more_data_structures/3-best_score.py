810731b580af772aea1f20736f0f266206c489e5
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-15 04:43:32 +0200

    Dictionary manupulations in py

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-        biggest_key = max(a_dictionary)
-        if a_dictionary == "":
-             return None
+        biggest_key = ord(a_dictionary)
+        if a_dictionary == " ":
+              return a_dictionary
+        elif a_dictionary == "":
+              return a_dictionary
         else:
             return biggest_key
