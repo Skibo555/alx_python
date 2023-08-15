64edf2ac9783deb7e9a13ec4ed6901fc2cdb4e69
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-15 04:05:25 +0200

    Dictionary manupulations in py

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -5,5 +5,5 @@ def update_dictionary(a_dictionary, key, value):
         a_dictionary[key] = value
         return a_dictionary
     else:
-        a_dictionary[key] = {}
+        a_dictionary[key] = value
         return a_dictionary
