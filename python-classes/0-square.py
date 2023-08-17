6e81d05f181e968e234eafbabe342d4bf6e6f033
Julius Yonwatode <yonwatodejulius@gmail.com>
2023-08-17 02:32:33 +0200

    The Classses with the documentations

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,11 @@
-"""
-python3 -c 'print(__import__("my_module").Square.__doc__)'
-"""
 class Square:
+    """
+    This is a sample class for demonstration purposes.
+
+    Attributes:
+        size (int): is empty
+    """
     def __init__(self, __size):
+        """
+        This method prints initializes the module we have.
+        """
