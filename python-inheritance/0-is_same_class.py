00745d4acabf29c76bd3819eb3277315def52ae7
Skibo555 <juliusyonwatade@gmail.com>
2023-08-20 06:17:13 +0000

    We are back to the days of old

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -3,8 +3,22 @@ Square Module
 
 This is a function that calculates squares using size as a parameter.
 """
-def is_same_class(obj, a_class):
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+class A:
+    """
+    A class representing a square.
+
+    Attributes:
+    - __size (int): The size of the square's sides.
+
+    Methods:
+    - __init__(self, size): Initializes a Square instance with the given size.
+
+    Example usage:
+    >>> square = Square(5)
+    """
+
+    def is_same_class(obj, a_class):
+        if isinstance(obj, a_class):
+            return True
+        else:
+            return False
