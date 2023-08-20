18ffa5d466cb285b67d42eedd810e79d6805a3f4
Skibo555 <juliusyonwatade@gmail.com>
2023-08-20 09:57:47 +0000

    We are back to the days of old

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,17 @@
+"""
+Square Module
+
+This is a function that calculates squares using size as a parameter.
+"""
+def is_kind_of_class(obj, a_class):
+    """
+    This is a docstring for is_same_class.
+
+    Args:
+        obj (int): The Object to be tested.
+        a_class (string): The input.
+
+    Returns:
+        bool: if the object is a class of the object returns True else it returns False.
+    """
+    return issubclass(type(obj), a_class)
