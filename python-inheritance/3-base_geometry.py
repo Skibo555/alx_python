ad01071d09da87daf868d03d9e4c3c631b36ed1a
Skibo555 <juliusyonwatade@gmail.com>
2023-08-20 10:15:50 +0000

    We are back to the days of old

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -5,4 +5,13 @@ This is a empty class
 """
 
 class BaseGeometry:
+    """
+    This is an empty class of Geometry.
+
+    Args:
+        There are no agruments as of now
+
+    Return:
+        It does not return anything for now but it will in future.
+    """
     pass
