241c820a0db7740b94740ee457f89c5d0c31ee9b
Skibo555 <juliusyonwatade@gmail.com>
2023-08-20 06:12:08 +0000

    We are back to the days of old

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,3 +1,8 @@
+"""
+Square Module
+
+This is a function that calculates squares using size as a parameter.
+"""
 def is_same_class(obj, a_class):
     if isinstance(obj, a_class):
         return True
