0b695f3f39ddf23aad17d101743aa252fc7846dc
Skibo555 <juliusyonwatade@gmail.com>
2023-08-20 23:40:32 +0000

    We are back to the days of old

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,35 @@
+"""
+Your module documentation goes here
+"""
+
+
+class MetaClass(type):
+    """
+    documentation
+    """
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=MetaClass):
+    """
+    documentation for class goes here
+    """
+
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+    def area(self):
+        """
+        Calculates the area and raises an exception.
+
+        Raises:
+            Exception: Always raises an exception.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if value is not int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
