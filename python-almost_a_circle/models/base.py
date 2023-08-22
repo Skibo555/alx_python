f2f4cff4c9e571039ec6c18d9c9c93a2aa5a5bef
Skibo555 <juliusyonwatade@gmail.com>
2023-08-22 20:11:48 +0000

    AirBnB clone

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,17 +1,25 @@
 """
-This is a module documentation.
-Though it is an empty one for now.
+Module: models.base
 
-I was asked to add more comments, I hope this is enough!
+This module defines the Base class.
+
+Classes:
+- Base: Represents the base class with an ID attribute.
 """
 
 class Base:
+    """
+    Base class with an ID attribute.
+
+    Attributes:
+        __nb_objects (int): Private class attribute to keep track of object count.
+        id (int): Public instance attribute representing the object's ID.
+
+    Methods:
+        __init__(self, id=None): Constructor to initialize the object's ID.
+    """
     __nb_objects = 0
     def __init__(self, id=None):
-        """
-        A module that's the constructor.
-        This module is cool.
-        """
         if id is not None:
             self.id = id
         else:
