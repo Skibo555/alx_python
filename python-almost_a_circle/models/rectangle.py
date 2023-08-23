8b7da4071b562290672610a314961736eca472d0
Skibo555 <juliusyonwatade@gmail.com>
2023-08-23 06:19:44 +0000

    Let see if the documentation works now

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -38,3 +38,55 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
+        @property
+    def get_width(self):
+        """
+        A getter for width
+        """
+        pass
+    @width.setter
+    def set_width(self, width):
+        """
+        A setter for the width attribute
+        """
+        pass
+
+    @property
+    def get_height(self):
+        """
+        A getter for height
+        """
+        pass
+    @height.setter
+    def set_height(self, height):
+        """
+        A setter for the height attribute
+        """
+        pass
+
+    @property
+    def get_x(self):
+        """
+        A getter for x
+        """
+        pass
+
+    @x.setter
+    def set_x(self, x):
+        """
+        A setter for the x attribute
+        """
+        pass
+    @property
+    def get_y(self):
+        """
+        A getter for y
+        """
+        pass
+    @y.setter
+    def set_y(self, y):
+        """
+        A setter for the y attribute
+        """
+        pass
