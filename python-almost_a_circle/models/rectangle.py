d52daf5ae1a033866223d6f7fa416bdf67aff5a0
Skibo555 <juliusyonwatade@gmail.com>
2023-08-24 09:51:05 +0000

    I love the fact that I had to go through this rigor

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -153,6 +153,8 @@ class Rectangle(Base):
         Returns:
             None
         """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("x  must be > 0")
         self.__x = value
@@ -178,6 +180,8 @@ class Rectangle(Base):
         Returns:
             None
         """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be > 0")
         self.__y = value
