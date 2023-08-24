6a1526af129bbc5486f9b894edd5d54c9af72a68
Skibo555 <juliusyonwatade@gmail.com>
2023-08-24 18:20:26 +0000

    I love the fact that I had to go through this rigor

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -235,7 +235,7 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
 
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
         The module is to update what we've got in the kwargs
         """
