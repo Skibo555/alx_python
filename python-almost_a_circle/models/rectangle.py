834ae200051b55daec25983d7ff0b07134c02a46
Skibo555 <juliusyonwatade@gmail.com>
2023-08-24 09:28:46 +0000

    I love the fact that I had to go through this rigor

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -38,6 +38,46 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+        self.validate_width()
+        self.validate_height()
+        self.validate_x()
+        self.validate_y()
+
+    def validate_width(self):
+        """
+        validates the arguments passed into the object
+        """
+        if not isinstance(self.__width, int):
+            raise TypeError("width must be an integer")
+        if self.__width <= 0:
+            raise ValueError("width must be > 0")
+
+    def validate_height(self):
+        """
+        validates the arguments passed into the object
+        """
+        if not isinstance(self.__height, int):
+            raise TypeError("height must be an integer")
+        if self.__height <= 0:
+            raise ValueError("height must be > 0")
+
+    def validate_x(self):
+        """
+        validates the arguments passed into the object
+        """
+        if not isinstance(self.__x, int):
+            raise TypeError("x must be an integer")
+        if self.__x < 0:
+            raise ValueError("x must be >= 0")
+
+    def validate_y(self):
+        """
+        validates the arguments passed into the object
+        """
+        if not isinstance(self.__y, int):
+            raise TypeError("y must be an integer")
+        if self.__y < 0:
+            raise ValueError("y must be >= 0")
 
     @property
     def width(self):
