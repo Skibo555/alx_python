86f7fd934126e84d8477983e49b12c3453c935ed
Skibo555 <juliusyonwatade@gmail.com>
2023-08-26 20:17:43 +0000

    Let us talk about networks

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,14 +5,14 @@ Request Module.
 This is the python defined module to fetch a URL.
 """
 from requests import requests
-"""
-This module fetches a url using.
+    """
+    This module fetches a url using.
 
-Arg:
-    urllib.request.Request module.
+    Arg:
+        urllib.request.Request module.
 
-Return:
-    Response.
+    Return:
+        Response.
     """
 work = urllib.request.Request('https://alu-intranet.hbtn.io/status')
 with urllib.request.urlopen(work) as response:
