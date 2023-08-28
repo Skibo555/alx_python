94fbcd8bbc9a770346b8cf1cb73fb53c4b85dd1c
Skibo555 <juliusyonwatade@gmail.com>
2023-08-28 20:14:08 +0000

    I want to understand the network more

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -17,35 +17,16 @@ def get_an_input():
         sys.agrv[1].
 
     """
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
+    if len(sys.argv) >= 1:
+        url = sys.argv[-1]
+        return url
+    else:
+        exit(1)
 
-        x_requests_id = response.headers.get('X-Request-Id')
-
-        if x_requests_id is not None:
-            return x_requests_id
-        else:
-            return None
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
-        return None
-def another():
+def get_the_positional_arg():
     """
-    This is a function that gets the argment from the command line.
-
-    Arg:
-        sys.argv[]
-
-    Return:
-        sys.argv[1]
+    This module is to the the last argument on the command line.
     """
-    if len(sys.argv) != 2:
-        sys.exit(1)
-
-    url = sys.argv[1]
-    x_request_id = get_an_input(url)
-
-
-    if x_request_id is not None:
-        print(x_request_id)
+    method = resquests.get(url)
+    view = method.header('X-Request-Id')
+    return view
