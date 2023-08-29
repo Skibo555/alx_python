1740af59bc8dcf952c8aa042dd905173081c9708
Skibo555 <juliusyonwatade@gmail.com>
2023-08-29 04:00:11 +0000

    I want to understand the network more

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -6,27 +6,6 @@ This is the python defined module to fetch a URL.
 import sys
 import requests
 
-def get_an_input():
-    """
-    This Module is to get the a url from the command line using.
-
-    Argument:
-        sys.agrv[]: The positional argument from the command line.
-    
-    Return:
-        sys.agrv[1].
-
-    """
-    if len(sys.argv) >= 1:
-        url = sys.argv[-1]
-        return url
-    else:
-        sys.exit(1)
-
-def get_the_positional_arg():
-    """
-    This module is to the the last argument on the command line.
-    """
-    method = requests.get(url)
-    view = method.headers.get('X-Request-Id')
-    return view
+url = sys.argv[-1]
+req = requests.get(url)
+req.headers('X-Request-Id')
