31931beac26265fc01305419798dd658e9ee0ad7
Skibo555 <juliusyonwatade@gmail.com>
2023-08-29 20:17:42 +0000

    I want to understand the network more

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -8,7 +8,7 @@ import requests
 # Get the letter from the command-line argument, or set it to an empty string if not provided
 if len(sys.argv) > 1:
     letter = sys.argv[1]
-if len(sys.argv) < 1:
+else:
     letter = ""
 
 # Define the URL
@@ -18,24 +18,24 @@ url = "http://0.0.0.0:5000/search_user"
 response = requests.post(url, data={'q': letter})
 
 # Check if the response body is properly JSON formatted
-if response.text.strip() and response.text.strip()[0] == "{" and response.text.strip()[-1] == "}":
-    # Parse the JSON-like response manually
-    response_parts = response.text.strip()[1:-1].split(",")
-    id_ = ""
-    name = ""
-    for part in response_parts:
-        key, value = part.split(":")
-        if key.strip() == '"id"':
-            id_ = value.strip('" ')
-        elif key.strip() == '"name"':
-            name = value.strip('" ')
+if response.text.strip():
+    if response.text.strip()[0] == "{" and response.text.strip()[-1] == "}":
+        # Parse the JSON-like response manually
+        response_parts = response.text.strip()[1:-1].split(",")
+        id_ = ""
+        name = ""
+        for part in response_parts:
+            key, value = part.split(":")
+            if key.strip() == '"id"':
+                id_ = value.strip('" ')
+            elif key.strip() == '"name"':
+                name = value.strip('" ')
 
-    if id_ and name:
-        print("[{}] {}".format(id_, name))
-    else:
-        print("No result")
-else:
-    if not response.text.strip():
-        print("No result")
+        if id_ and name:
+            print("[{}] {}".format(id_, name))
+        else:
+            print("No result")
     else:
         print("Not a valid JSON")
+else:
+    print("No result")
