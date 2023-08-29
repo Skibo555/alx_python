f72e3e349e35a2d2f0261ebe567e4e59bf022cf2
Skibo555 <juliusyonwatade@gmail.com>
2023-08-29 18:48:39 +0000

    I want to understand the network more

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,13 @@
+"""
+This is a module that gets a request.
+"""
+
+import sys
+import requests
+
+url = sys.argv[-1]
+
+req = requests.get(url)
+
+if req.status_code >= 400:
+    print("Error code:{%d}".format(req.status_code))
