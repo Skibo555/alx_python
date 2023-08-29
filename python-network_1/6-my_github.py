ada881874c70c62d4c4742420120cf9372570dbf
Skibo555 <juliusyonwatade@gmail.com>
2023-08-29 21:03:42 +0000

    My GitHub

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,14 +1,14 @@
 """
- Python script that takes your GitHub credentials
-   (username and password) and uses the GitHub API to display your id
+A module to my GitHub.
 """
-import sys 
+
+import sys
 import requests
 
 url = 'https://api.github.com/user'
 username = sys.argv[1]
 password = sys.argv[2]
-parameters = {username, password)
+parameters = (username, password)
 
 response = requests.get(url, auth=parameters)
 try:
