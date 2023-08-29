b0b16899951c03c8e4bab9f8f12857c8dbeca7d0
Skibo555 <juliusyonwatade@gmail.com>
2023-08-29 17:51:47 +0000

    I want to understand the network more

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -7,19 +7,12 @@ import sys
 import requests
 
 # Get the URL from the command-line arguments
-url = sys.argv[1]
+url = sys.argv[-1]
 
 # Send a request and get the response
 response = requests.get(url)
 
 # Check if the request was successful
-if response.status_code == 200:
-    # Get the value of the X-Request-Id header
-    x_request_id = response.headers.get('X-Request-Id')
 
-    if x_request_id is not None:
-        print("X-Request-Id:", x_request_id)
-    else:
-        print("X-Request-Id header not found in the response.")
-else:
-    print("Request was not successful. Status code:", response.status_code)
+if response.status_code == 200:
+    print(response.headers.get('X-Request-Id')
