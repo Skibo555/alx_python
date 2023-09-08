60dae2020cd2ef25f6cc14c489e76a173b76cae2
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-08 21:32:09 +0100

    First time that I can write a code and send it to my GitHub with my Local machine

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+"""
+This is a SQL query using python script.
+"""
+import sys
+import MySQLdb
+
+
+def sql():
+    """
+    Here's the logic to get arguments from the command line.
+    """
+    args = sys.argv[1:]
+    if len(args) <= 2:
+        arg1 = [0]
+        arg2 = [1]
+        arg3 = [-1]
+    db = MySQLdb.connect(
+        host="localhost",
+        user=arg1,
+        passwd=arg2,
+        db=arg3
+    )
+    db_cursor = db_connect.cursor()
+    db_cursor.execute("SELECT * FROM states")
+    db_cursor.execute("ORDER BY states.id [ASC|DESC]")
+
+
+if __name__ == '__main__':
+    sql()
