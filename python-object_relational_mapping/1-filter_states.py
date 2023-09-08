624ba41d353065b2a4b7cd0e14812bc1c6dd2aad
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-08 23:36:32 +0100

    First time that I can write a code and send it to my GitHub with my Local machine

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,43 @@
+"""
+This is a SQL query using python script.
+"""
+import sys
+import MySQLdb
+
+
+def list_states(username, password, database_name):
+    """
+    Here's the logic to get arguments from the command line.
+    """
+    try:
+        # Connect to the MySQL server on localhost at port 3306
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database_name
+        )
+
+        # Create a cursor object to execute SQL queries
+        cursor = db.cursor()
+
+        # Retrieve all states from the 'states' table and order by 'id' in ascending order
+        cursor.execute(
+            "SHOW * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+        # Fetch and print the results
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database_name = sys.argv[3]
+        list_states(username, password, database_name)
