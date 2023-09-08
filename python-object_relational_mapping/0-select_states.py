b13254b6d3fff76123bcb29dd48c878acea5eda9
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-08 23:05:16 +0100

    First time that I can write a code and send it to my GitHub with my Local machine

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,23 +16,28 @@ def sql():
     arg1 = sys.argv[1]
     arg2 = sys.argv[2]
     arg3 = sys.argv[3]
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=arg1,
-        passwd=arg2,
-        db=arg3
-    )
-    db_cursor = db.cursor()
-    db_cursor.execute("SELECT * FROM states")
-    db_cursor.execute("ORDER BY states.id ASC")
-    result = db_cursor.fetchall()
-    for row in result:
-        print(row)
 
-    db.commit()
-    db_cursor.close()
-    db.close()
+    try:
+
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=arg1,
+            passwd=arg2,
+            db=arg3
+        )
+        db_cursor = db.cursor()
+        db_cursor.execute("SELECT * FROM states")
+        db_cursor.execute("ORDER BY states.id ASC")
+        result = db_cursor.fetchall()
+        for row in result:
+            print(row)
+        db.commit()
+        db_cursor.close()
+        db.close()
+
+    except Exception as q:
+        return ("{}, shey you no see ni?".format(q))
 
 
 if __name__ == '__main__':
