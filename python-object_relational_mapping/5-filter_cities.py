42ba4b1591246af34897920c1afa604509fedcf4
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-09 21:57:25 +0100

    We are here again to do hard things

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+"""
+This is a script thaat queries the database and it's a SQL injection free!
+"""
+
+import sys
+import MySQLdb
+
+
+def injection_free():
+    if len(sys.argv) != 5:
+        exit
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database_name = sys.argv[3]
+        state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name
+    )
+
+    obj = db.cusor()
+
+    query = "SELECT * cities"
+    result = obj.excute(query, "FORM", state_name)
+
+    db.close()
+    for row in result:
+        print(row)
+
+
+if __name__ == '__main__':
+    injection_free()
