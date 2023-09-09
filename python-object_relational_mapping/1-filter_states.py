aa7653bbbb9c2b7ee36afd0bb8d333fade11b60f
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-09 03:45:29 +0100

    First time that I can write a code and send it to my GitHub with my Local machine

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -24,7 +24,7 @@ def list_states(username, password, database_name):
 
         # Retrieve all states from the 'states'.
         cursor.execute(
-            "SELECT * FROM `states` WHERE `name` REGEXP BINARY '[N]' ORDER BY id ASC")
+            "SELECT * FROM states WHERE name COLLATE utf8mb4_bin LIKE 'N%' ORDER BY id ASC")
 
         # Fetch and print the results
         results = cursor.fetchall()
