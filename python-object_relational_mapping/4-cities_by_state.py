5b7ad40c60bb4c6b0f378db08daa6c5237ab4636
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-09 21:29:00 +0100

    We are here again to do hard things

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -23,9 +23,9 @@ def sql_query():
         )
 
         sql = db.cursor()
-        query = "SELECT cities.id, cities.name, states.name \
-                   FROM cities, states \
-                   WHERE states.id = state_id ORDER BY cities.id"
+        query = ("SELECT cities.id, cities.name, states.name \
+                FROM cities, states \
+                WHERE states.id = state_id ORDER BY cities.id")
 
         sql.execute(query)
         result = sql.fetchall()
