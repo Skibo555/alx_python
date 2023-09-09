922a630efd6d7e6941dd6560b32bc5b19a5c5bdb
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-09 11:27:14 +0100

    We are here again to do hard things

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ def sql():
         username = sys.argv[1]
         password = sys.argv[2]
         database_name = sys.argv[3]
-        search = sys.argv[4]
+        search = sys.argv[4].format()
 
     connection = MySQLdb.connect(
         host="localhost",
@@ -24,11 +24,11 @@ def sql():
     )
     db_cur = connection.cursor()
 
-    sql_query = ("SELECT * FROM states WHERE name = format(%s) \
-            ORDER BY states.id ASC")
-    result = db_cur.execute(sql_query)
+    sql_query = ("SELECT * FROM states WHERE name=%s \
+            ORDER BY id ASC")
+    db_cur.execute(sql_query, (search,))
 
-    results = result.fetchall()
+    results = db_cur.fetchall()
     for row in results:
         print(row)
 
