8bca5ca96d7c53a1db0d55c9a719487f6fe8110b
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-09 11:11:43 +0100

    We are here again to do hard things

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,26 +5,36 @@ This is a module that gets fucks done on a SQL server.
 import sys
 import MySQLdb
 
+
+def sql():
+    if len(sys.argv) != 5:
+        exit
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database_name = sys.argv[3]
+        search = sys.argv[4]
+
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name
+    )
+    db_cur = connection.cursor()
+
+    sql_query = ("SELECT * FROM states WHERE name = format(%s) \
+        FROM states WHERE name='search' \
+        ORDER BY id ASC")
+    result = db_cur.execute(sql_query)
+
+    results = result.fetchall()
+    for row in results:
+        print(row)
+
+    connection.close()
+
+
 if __name__ == '__main__':
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-    search = sys.argv[4]
-
-connection = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=username,
-    passwd=password,
-    db=database_name
-)
-db_cur = connection.cursor()
-sql_query = ("SELECT FORMAT(name, 'search') AS actual_search \
-    FROM states WHERE name='search'")
-result = sql_query.execute(sql_query)
-
-results = result.fetchall()
-for row in results:
-    print(row)
-connection.close()
+    sql()
