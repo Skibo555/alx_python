bf27b02f8a2e5d59c82c9ad774b54029ccfbd5c5
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-09 04:27:11 +0100

    First time that I can write a code and send it to my GitHub with my Local machine

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+"""
+This is a module that gets fucks done on a SQL server.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    search = sys.argv[4]
+
+connection = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=username,
+    passwd=password,
+    db=database_name
+)
+db_cur = connection.cursor()
+sql_query = "SELECT * FROM states"
+cursor.execute(sql_query, (search))
+
+result = cursor.fetchall()
+for row in result:
+    print(row)
+connection.close()
