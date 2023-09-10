f8b63aad26e8ed4cfad2926b56e30d4c8377529b
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-10 13:32:43 +0100

    SQLAlchemy

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,16 +1,12 @@
 from sqlalchemy import create_engine, Column, Integer, String
-
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-"""
-I don't know what the checker is looking for but take this.
-"""
 
-
+# A class to create the tables
 class State(Base):
-    """
+    '''
     A class that models the table 'states', inherited from Base
 
     Attributes:
@@ -18,10 +14,13 @@ class State(Base):
         id(int): unique identity number of each state
 
         name(string): name of each state
-    """
+    '''
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True, autoinrement=True, nullable=False)
-    name = Column(String, Integer(128), nullable=False)
+    id = Column(Integer, autoincrement=True,
+                unique=True,
+                nullable=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
 
     def __init__(self, name):
         self.name = name
