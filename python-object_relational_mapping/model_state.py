1fd2b127b2ebd57ec0feb9e0a6a999858b69b7de
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-10 13:40:44 +0100

    SQLAlchemy

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,23 +1,31 @@
 from sqlalchemy import create_engine, Column, Integer, String
+
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+# The class documentations
 
-# A class to create the tables
+"""
+The class documentation.
+"""
+
+
 class State(Base):
-    '''
+    """
     A class that models the table 'states', inherited from Base
 
     Attributes:
 
-        id(int): unique identity number of each state
+        id(int): unique identity number a state
 
-        name(string): name of each state
-    '''
+        name(string): name of state
+    """
     __tablename__ = "states"
-    id = Column(Integer, autoincrement=True, nique=True,
-                nullable=False, primary_key=True)
+    id = Column(Integer, unique=True,
+                primary_key=True,
+                autoinrement=True,
+                nullable=False)
     name = Column(String(128), nullable=False)
 
     def __init__(self, name):
