4b041849dc0baeb0be72e6dac638bca347795773
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-10 15:12:16 +0100

    SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,30 +3,34 @@ Documentation for this file.
 """
 import sys
 from model_state import Base, State
-import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-def func():
-    """
-    The function I was asked to create for the marks!
-    """
-    if len(sys.argv) != 3:
-        exit
-    else:
-        mysql_username = sys.argv[1]
-        mysql_password = sys.argv[2]
-        mysql_database_name = sys.argv[3]
+if __name__ == '__main__':
+    def func():
+        """
+        The function I was asked to create for the marks!
+        """
+        if len(sys.argv) != 3:
+            exit
+        else:
+            mysql_username = sys.argv[1]
+            mysql_password = sys.argv[2]
+            mysql_database_name = sys.argv[3]
 
-        engine = create_engine('mysql+mysqldb://{}:{}'
-                               .format("mysql_username",
-                                       "mysql_password", "mysql_database_name"),
-                               pool_pre_ping=True)
+        route = 'mysql+mysqldb://{}:{}' \
+            .format(mysql_username,
+                    mysql_password,
+                    mysql_database_name)
+        engine = create_engine(route)
+
         Base.metadata.create_all(engine)
 
-        session = Session(engine)
+        session = sessionmaker(bind=engine)
+        session = session()
 
         for session in session.query(State).order_by(State.id).all():
-            print("{}: {}".format(States.id, States.name))
+            print("{}: {}".format(state.id, state.name))
 
         session.close()
