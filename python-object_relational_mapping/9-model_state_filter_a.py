69eaa0f085eca1438322e90c5b34822d5c18ef13
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-11 10:55:56 +0100

    SQLAlchemy

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,33 @@
+"""
+Documentation for this file.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_database_name = sys.argv[3]
+
+    route = 'mysql+mysqldb://{}: {}@localhost/{}' \
+        .format(mysql_username,
+                mysql_password,
+                mysql_database_name)
+
+    engine = create_engine(route)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(State.id).all()
+
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+
+    session.close()
