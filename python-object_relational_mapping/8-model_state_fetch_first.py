b7e7169644f25ad07b1051c5333219e9a3f85048
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-11 06:12:47 +0100

    SQLAlchemy

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+"""
+Documentation for this file.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_database_name = sys.argv[3]
+
+    route = 'mysql+mysqldb://{}: {}@localhost/{}' \
+        .format(mysql_username,
+                mysql_password,
+                mysql_database_name)
+
+    engine = create_engine(route)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name[0]))
+
+    session.close()
