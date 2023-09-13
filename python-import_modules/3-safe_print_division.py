6edc649c8756f227ee85ab859c01463553d8ee00
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-13 18:49:23 +0100

    the works on data structures

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -3,12 +3,10 @@
 def safe_print_division(a, b):
     try:
         i = a / b
-    except ZeroDivisionError as a:
-        print("None")
-        return (a)
-    except TypeError as b:
-        print("None")
-        return (b)
+    except ZeroDivisionError:
+        return None
+    else:
+        return i
     finally:
         print("Inside result: {}".format(i))
         return (i)
