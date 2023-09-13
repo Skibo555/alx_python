618e4e6efb2a66589224bc1e2cbbe1cf63f10dfd
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-09-13 08:44:34 +0100

    Web framework, such as Flask

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -10,4 +10,8 @@ app = Flask(__name__)
 @app.route("/")
 def display():
     return "Hello HBNB!"
-# app.run(strict_slashes=False)
+
+
+if __name__ == '__main__':
+
+    app.run(strict_slashes=False, host='0.0.0.0', port=5000)
