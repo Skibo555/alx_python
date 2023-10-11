83aac5cd445ac77403c9d71a6cc90a8e84df0046
Skibo555 <104285668+Skibo555@users.noreply.github.com>
2023-10-11 10:40:53 +0100

    I have to pass this task

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -20,30 +20,21 @@ def get_items(employee_id):
         user_data = response.json()
         todo_data = todo_list.json()
 
+        # Prepare data for CSV
         employee_name = user_data.get("name", "Unknown Employee")
-        employee_username = user_data.get("username", "Unknown Username")
-
-        # Create a list to store the CSV rows
         csv_data = []
 
         for task in todo_data:
-            task_status = "Completed" if task["completed"] else "Not Completed"
+            # Convert task status to "Completed" or "Not Completed"
+            status = "True" if task["completed"] else "False"
             task_title = task["title"]
-            csv_data.append([employee_id, employee_name,
-                            employee_username, task_status, task_title])
+            csv_data.append([employee_id, employee_name, status, task_title])
 
         # Print the todo progress
-        print("Employee {} is done with tasks({}/{}):".format(employee_name,
-              len(todo_data), len(todo_data)))
-        for task in csv_data:
-            print("\t {}, {}, {}, {}".format(
-                task[0], task[1], task[3], task[4]))
-
-        # Write the data to a CSV file
-        with open('{}_tasks.csv'.format(employee_id), 'w', newline='') as csvfile:
-            my_writer = csv.writer(csvfile)
-            my_writer.writerow(
-                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        with open('USER_ID.csv', 'w', newline='') as csvfile:
+            my_writer = csv.writer(csvfile, delimiter=',')
+            my_writer.writerow(["USER_ID", "EMPLOYEE_NAME",
+                               "TASK_STATUS", "TASK_TITLE"])
             my_writer.writerows(csv_data)
     else:
         print("Failed to retrieve employee information and TODO list.")
